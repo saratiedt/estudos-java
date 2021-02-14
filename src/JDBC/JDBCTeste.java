5a9a65fd102e26f27213e62e3aa52d053cf6330e
Sara Cristine Tiedt (GOVBR MTZ - DDP Arquitetura) <Sara.Tiedt@govbr.com.br>
2021-02-14 00:42:47 -0300

    aula preparedStatement

--- src/JDBC/JDBCTeste.java
+++ src/JDBC/JDBCTeste.java
@@ -2,6 +2,8 @@ package JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,12 +11,47 @@ public class JDBCTeste {
 
 	public static void main(String[] args) {
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/exemplo", "root", "root");
+			String url = "jdbc:mysql://localhost/exemplo?useSSL=false";
+			String user = "root";
+			String password = "root";
+
+			Connection connection = DriverManager.getConnection(url, user, password);
 			Statement statement = connection.createStatement();
-			//String sql = "insert into pessoa values (3, 'Gonsalves', 'Alex', 22, 'desenvolvedor');";
-			//String sql = "update pessoa set idade=23 where id=3;";
-			String sql = "delete pessoa where id=3;";
-			statement.executeUpdate(sql);
+			String primeiroNome = "Maria";
+			String profissao = "Vendedora";
+			String ultimoNome = "Cecília";
+			int idade = 30;
+			int id = 4;
+			String sql = "insert into pessoa values (?,?,?,?,?);";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			preparedStatement.setString(2, ultimoNome);
+			preparedStatement.setString(3, primeiroNome);
+			preparedStatement.setInt(4, idade);
+			preparedStatement.setString(5, profissao);
+			preparedStatement.execute();
+
+			statement.execute(sql);
+			// 'desenvolvedor');";
+			// String sql = "update pessoa set idade=23 where id=3;";
+			// String sql = "delete from pessoa where id=3;";
+
+//			String sql = "select * from pessoa";
+//
+//			ResultSet resultSet = statement.executeQuery(sql);
+//
+//			for (int i = 1; i < resultSet.getMetaData().getColumnCount(); i++) 
+//				System.out.print(resultSet.getMetaData().getColumnName(i)+ " ");
+//				System.out.println();
+//			
+//			
+//			while (resultSet.next()) {
+//				for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+//					System.out.print(resultSet.getString(i) + " ");
+//				}
+//				System.out.println();
+//
+//			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
